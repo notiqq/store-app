edd945e81d4dd2b855d8236ce1ac4f7b4912b7c9
Andrii <not4me2009@gmail.com>
2020-08-07 14:06:25 +0300

    update

--- server/project/app/admin_blueprint.py
+++ server/project/app/admin_blueprint.py
@@ -15,31 +15,31 @@ def product_list():
 
 @admin_blueprint.route('/admin/product/add', methods=['GET','POST'])
 def product_add():
-
     if request.method == 'POST':
-        post_data = request.get_json()
-        name = post_data.get('name')
-        price = post_data.get('price')
-        image_url = post_data.get('image_url')
+        name = request.form['name']
+        price = request.form['price']
+        image_url = request.form['image_url']
         db.session.add(Product(name=name, price=price, image_url=image_url))
         db.session.commit()
         return redirect("/admin", code=303)
     
     return render_template("pages/admin/product/add.html")
 
-@admin_blueprint.route('/admin/product/edit/<product_id>', methods=['GET','POST'])
+@admin_blueprint.route('/admin/product/edit/<product_id>', methods=['GET'])
 def product_edit(product_id):
-  
     product = Product.query.filter_by(id=product_id).first()
-    if request.method == 'GET':
-        return render_template("pages/admin/product/edit.html")
+    if product == None:
+        return redirect("/admin", code=303)
+    return render_template("pages/admin/product/edit.html", product = product)
 
-    if request.method == 'POST':
-        post_data = request.get_json()
-        product.name = post_data.get('name')
-        product.price = post_data.get('price')
-        product.image_url = post_data.get('image_url')
-        db.session.commit()
+@admin_blueprint.route('/admin/product/edit', methods=['POST'])
+def product_edit_post():
+
+    product = Product.query.filter_by(id=request.form['id']).first()
+    product.name = request.form['name']
+    product.price = request.form['price']
+    product.image_url = request.form['image_url']
+    db.session.commit()
     return redirect("/admin", code=303)
 
 @admin_blueprint.route('/admin/product/delete/<product_id>', methods=['GET'])
